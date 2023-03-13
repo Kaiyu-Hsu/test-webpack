b3dab372c89547d13c14d0da2f91577ac6e2fe68
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2023-03-13 10:20:58 +0800

    add: child component for test

---
+++ src/components/Child.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>this is child component</div>
+</template>
+
+<script>
+const path = require("path");
+const pathResolve = path.resolve(__dirname); // 取得當前資料夾的絕對路徑
+const pathJoin = path.join(__dirname);
+
+export default {
+  mounted() {
+    console.log(path.dirname(__filename));
+  },
+};
+</script>
